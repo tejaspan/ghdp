1b3fe99a4dae7f1f0a4cd3f59e0c0f9d10d71aeb
hardik patel <panchaltn@gmail.com>
2019-04-09 12:53:34 -0700

    seperate code

--- ghdp.sh
+++ ghdp.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 
 #variables
@@ -16,8 +16,5 @@ proj_name=api-project-18744604383
 
 
 #creating instances
-for ((i=1;i<=$t_node;i++))
-do
-	echo "Creating VM instance-$i"
-	echo "compute --project=$proj_name instances create instance-$i --zone=us-central1-c --machine-type=n1-standard-4 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=18744604383-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=cloudera-hadoop-web-ui,http-server,https-server --image=rhel-6-v20190326 --image-project=rhel-cloud --boot-disk-size=10GB --boot-disk-type=pd-ssd --boot-disk-device-name=instance-$i"
-done
+bash deploy.sh $t_node $proj_name
+
---
+++ take_ssh.sh
@@ -0,0 +1,4 @@
+#generating ssh-key & importing for all machine
+
+
+gcloud compute ssh panchaltn@instance-1
